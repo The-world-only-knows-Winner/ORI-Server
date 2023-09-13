07e17cd41d47481dc3b3f7002322dbfbd06ea946
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-13 09:44:10 +0900

    📑 :: FeignException들 추가

---
+++ ori-infrastructure/src/main/kotlin/com/onlywin/ori/common/feign/exception/FeignException.kt
@@ -0,0 +1,24 @@
+package com.onlywin.ori.common.feign.exception
+
+import com.onlywin.ori.common.error.OriException
+import com.onlywin.ori.common.feign.error.FeignErrorCode
+
+object FeignBadRequestException : OriException(
+    FeignErrorCode.FEIGN_BAD_REQUEST,
+)
+
+object FeignUnAuthorizedException : OriException(
+    FeignErrorCode.FEIGN_UNAUTHORIZED,
+)
+
+object FeignExpiredTokenException : OriException(
+    FeignErrorCode.FEIGN_EXPIRED_TOKEN,
+)
+
+object FeignForbiddenException : OriException(
+    FeignErrorCode.FEIGN_FORBIDDEN,
+)
+
+object FeignInternalServerErrorException : OriException(
+    FeignErrorCode.FEIGN_INTERNAL_SERVER_ERROR,
+)
