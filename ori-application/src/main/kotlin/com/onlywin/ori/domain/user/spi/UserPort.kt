53bb27ff5fd6c513be4f0b6bc97ac1936db3d00f
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-17 17:14:52 +0900

    ♻️ :: Command, Query 순서 변경

--- ori-application/src/main/kotlin/com/onlywin/ori/domain/user/spi/UserPort.kt
+++ ori-application/src/main/kotlin/com/onlywin/ori/domain/user/spi/UserPort.kt
@@ -5,12 +5,12 @@ import java.util.UUID
 
 interface UserPort : CommandUserPort, QueryUserPort
 
+interface CommandUserPort {
+    fun saveUser(user: User): User
+}
+
 interface QueryUserPort {
     fun queryUserById(id: UUID): User?
     fun queryUserByEmail(email: String): User?
     fun existsUserByEmail(email: String): Boolean
-}
-
-interface CommandUserPort {
-    fun saveUser(user: User): User
 }
