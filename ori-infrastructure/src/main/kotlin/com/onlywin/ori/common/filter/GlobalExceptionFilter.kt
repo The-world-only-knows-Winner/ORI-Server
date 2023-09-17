270cdae7e2cbe7b4f5b1d60cf8bfbf292b38a5ca
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-17 20:58:41 +0900

    ♻️ :: Filter 로직 변경

--- ori-infrastructure/src/main/kotlin/com/onlywin/ori/common/filter/GlobalExceptionFilter.kt
+++ ori-infrastructure/src/main/kotlin/com/onlywin/ori/common/filter/GlobalExceptionFilter.kt
@@ -2,14 +2,14 @@ package com.onlywin.ori.common.filter
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.onlywin.ori.common.error.ErrorProperty
-import com.onlywin.ori.common.error.GlobalErrorCode
 import com.onlywin.ori.common.error.OriException
 import com.onlywin.ori.common.error.response.ErrorResponse
-import org.springframework.web.filter.OncePerRequestFilter
+import com.onlywin.ori.common.exception.InternalServerErrorException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.MediaType
+import org.springframework.web.filter.OncePerRequestFilter
 import java.nio.charset.StandardCharsets
 
 class GlobalExceptionFilter(
@@ -23,11 +23,17 @@ class GlobalExceptionFilter(
     ) {
         try {
             filterChain.doFilter(request, response)
-        } catch (e: OriException) {
-            writeErrorResponse(response, e.errorProperty)
         } catch (e: Exception) {
-            e.printStackTrace()
-            writeErrorResponse(response, GlobalErrorCode.INTERNAL_SERVER_ERROR)
+            when (e.cause) {
+                is OriException -> {
+                    writeErrorResponse(response, (e.cause as OriException).errorProperty)
+                }
+
+                is Exception -> {
+                    e.printStackTrace()
+                    writeErrorResponse(response, InternalServerErrorException.errorProperty)
+                }
+            }
         }
     }
 
@@ -36,13 +42,7 @@ class GlobalExceptionFilter(
             status = errorProperty.status
             contentType = MediaType.APPLICATION_JSON_VALUE
             characterEncoding = StandardCharsets.UTF_8.name()
+            writer.write(objectMapper.writeValueAsString(ErrorResponse(errorProperty.status, errorProperty.message)))
         }
-        val errorResponse = ErrorResponse(
-            status = errorProperty.status,
-            message = errorProperty.message,
-        )
-        response.writer.write(
-            objectMapper.writeValueAsString(errorResponse),
-        )
     }
 }
