53c238cc9c4f3b73fa5aae139f777e34a4031bcb
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-30 22:07:35 +0900

    ⚡️ :: 로그아웃 api

--- ori-infrastructure/src/main/kotlin/com/onlywin/ori/domain/auth/presentation/AuthWebAdapter.kt
+++ ori-infrastructure/src/main/kotlin/com/onlywin/ori/domain/auth/presentation/AuthWebAdapter.kt
@@ -4,14 +4,18 @@ import com.onlywin.ori.domain.auth.dto.response.TokenResponse
 import com.onlywin.ori.domain.auth.presentation.dto.request.SendAuthCodeWebRequest
 import com.onlywin.ori.domain.auth.presentation.dto.request.SignInWebRequest
 import com.onlywin.ori.domain.auth.presentation.dto.request.VerifyAuthCodeWebRequest
+import com.onlywin.ori.domain.auth.usecase.LogOutUseCase
 import com.onlywin.ori.domain.auth.usecase.SendAuthCodeUseCase
 import com.onlywin.ori.domain.auth.usecase.SignInUseCase
 import com.onlywin.ori.domain.auth.usecase.VerifyAuthCodeUseCase
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/auth")
@@ -20,6 +24,7 @@ class AuthWebAdapter(
     private val sendAuthCodeUseCase: SendAuthCodeUseCase,
     private val verifyAuthCodeUseCase: VerifyAuthCodeUseCase,
     private val signInUseCase: SignInUseCase,
+    private val logOutUseCase: LogOutUseCase,
 ) {
 
     @PostMapping("/code")
@@ -41,5 +46,11 @@ class AuthWebAdapter(
     @PostMapping("/token")
     fun signIn(@RequestBody request: SignInWebRequest): TokenResponse {
         return signInUseCase.execute(request.toDomainRequest())
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/logout")
+    fun logOut() {
+        logOutUseCase.execute()
     }
 }
