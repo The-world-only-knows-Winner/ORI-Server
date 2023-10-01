7d3b5571ad16d0098ada4ab92b36fab066175500
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-10-01 17:33:04 +0900

    ⚡️ :: saveAndGetId 메소드 추가

--- ori-application/src/main/kotlin/com/onlywin/ori/domain/route/spi/RoutePort.kt
+++ ori-application/src/main/kotlin/com/onlywin/ori/domain/route/spi/RoutePort.kt
@@ -1,10 +1,14 @@
 package com.onlywin.ori.domain.route.spi
 
+import com.onlywin.ori.domain.route.Route
 import com.onlywin.ori.domain.route.dto.response.QueryRouteList.RouteElement
+import java.util.UUID
 
 interface RoutePort : CommendRoutePort, QueryRoutePort
 
-interface CommendRoutePort
+interface CommendRoutePort {
+    fun saveRouteAndGetId(route: Route): UUID
+}
 
 interface QueryRoutePort {
     fun queryPublicTransitRouteByPoint(startXPoint: Float, startYPoint: Float, endXPoint: Float, endYPoint: Float): RouteElement
