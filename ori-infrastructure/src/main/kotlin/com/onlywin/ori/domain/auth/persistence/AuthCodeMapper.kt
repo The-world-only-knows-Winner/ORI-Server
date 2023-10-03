08439608c2e7b2807cb4f338d34184dc40157067
gilgeunwoo <kesksjxhx@gmail.com>
2023-10-03 23:24:22 +0900

    🐛 :: mapstruct 버그

--- ori-infrastructure/src/main/kotlin/com/onlywin/ori/domain/auth/persistence/AuthCodeMapper.kt
+++ ori-infrastructure/src/main/kotlin/com/onlywin/ori/domain/auth/persistence/AuthCodeMapper.kt
@@ -2,8 +2,9 @@ package com.onlywin.ori.domain.auth.persistence
 
 import com.onlywin.ori.domain.auth.AuthCode
 import org.mapstruct.Mapper
+import org.mapstruct.ReportingPolicy
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 abstract class AuthCodeMapper {
     abstract fun authCodeDomainToEntity(authCode: AuthCode): AuthCodeEntity
     abstract fun authCodeEntityToDomain(authCodeEntity: AuthCodeEntity?): AuthCode?
--- ori-infrastructure/src/main/kotlin/com/onlywin/ori/domain/user/persistence/UserMapper.kt
+++ ori-infrastructure/src/main/kotlin/com/onlywin/ori/domain/user/persistence/UserMapper.kt
@@ -2,8 +2,9 @@ package com.onlywin.ori.domain.user.persistence
 
 import com.onlywin.ori.domain.user.User
 import org.mapstruct.Mapper
+import org.mapstruct.ReportingPolicy
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 abstract class UserMapper {
     abstract fun userDomainToEntity(user: User): UserEntity
     abstract fun userEntityToDomain(userEntity: UserEntity?): User?
