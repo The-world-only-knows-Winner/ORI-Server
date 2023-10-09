5e893681371844feff1bd862ae5da9d1616237f1
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-10-09 17:27:04 +0900

    🔀 :: email from 설정

--- ori-infrastructure/src/main/kotlin/com/onlywin/ori/thirdparty/jms/SendMailAdapter.kt
+++ ori-infrastructure/src/main/kotlin/com/onlywin/ori/thirdparty/jms/SendMailAdapter.kt
@@ -2,17 +2,21 @@ package com.onlywin.ori.thirdparty.jms
 
 import com.onlywin.ori.common.annotation.Adapter
 import com.onlywin.ori.common.spi.SendMailPort
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 
 @Adapter
 class SendMailAdapter(
     private val javaMailSender: JavaMailSender,
+    @Value("\${spring.mail.username}")
+    private val oriEmail: String,
 ) : SendMailPort {
 
     override fun sendMail(title: String, content: String, email: String) {
         val message = SimpleMailMessage().apply {
             setTo(email)
+            from = oriEmail
             subject = title
             text = content
         }
